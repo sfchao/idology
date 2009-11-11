a488dd45a1228fe97e1fea4a8ad756c895a3de2f
Daniel Morrison <daniel@collectiveidea.com>
2009-11-11 11:57:27 +0000

    Added a Rakefile and moving files around in preparation for gemification.

---
+++ lib/idology.rb
@@ -0,0 +1,4 @@
+require File.dirname(__FILE__) + "/../base"
+require File.dirname(__FILE__) + "/base"
+require File.dirname(__FILE__) + "/api_request"
+require File.dirname(__FILE__) + "/api_response"
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,26 +1,32 @@
+$LOAD_PATH.unshift(File.dirname(__FILE__))
+$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 require 'rubygems'
+require 'idology'
 require 'spec'
+require 'spec/autorun'
 
-require File.dirname(__FILE__) + "/../../id_verification"
+Spec::Runner.configure do |config|
+
+end
 
 module RequestSpecHelper
   def test_subject
     subject = API::IDVerification::Subject.new(
       {
         # basic info
-        :firstName => 'Test', 
-        :lastName => 'Person', 
-        :address => '123 Main St', 
-        :city => 'New York', 
-        :state => 'NY', 
-        :zip => 10001, 
-        :ssnLast4 => 1234, 
-        :dobMonth => 1, 
+        :firstName => 'Test',
+        :lastName => 'Person',
+        :address => '123 Main St',
+        :city => 'New York',
+        :state => 'NY',
+        :zip => 10001,
+        :ssnLast4 => 1234,
+        :dobMonth => 1,
         :dobYear => 1980,
-        :userID => 1,        
+        :userID => 1,
       }
     )
-    
+
     # more for aditional tests
     subject.idNumber = 12345
     subject.verification_questions = test_verification_questions
@@ -29,10 +35,10 @@ module RequestSpecHelper
 
     return subject
   end
-  
+
   def test_verification_questions
     questions = []
-    
+
     # question 1 with answers
     q = API::IDVerification::Question.new
     q.prompt = "TEST - With which name are you associated?"
@@ -45,7 +51,7 @@ module RequestSpecHelper
     q.candidate_answers << API::IDVerification::Answer.new("None of the above")
     q.chosen_answer = "JANNE"
     questions << q
-    
+
     # question 2 with answers
     q = API::IDVerification::Question.new
     q.prompt = "TEST - Which number goes with your address on CARVER BLVD?"
@@ -58,7 +64,7 @@ module RequestSpecHelper
     q.candidate_answers << API::IDVerification::Answer.new("None of the above")
     q.chosen_answer = "142"
     questions << q
-    
+
     # question 3 with answers
     q = API::IDVerification::Question.new
     q.prompt = "TEST - Which cross street is near your address on HALBURTON RD?"
@@ -71,13 +77,13 @@ module RequestSpecHelper
     q.candidate_answers << API::IDVerification::Answer.new("None of the above")
     q.chosen_answer = "MEADOW ST"
     questions << q
-        
-    return questions    
+
+    return questions
   end
 
   def test_challenge_verification_questions
     questions = []
-    
+
     # question 1 with answers
     q = API::IDVerification::Question.new
     q.prompt = "TEST CHALLENGE - With which name are you associated?"
@@ -90,7 +96,7 @@ module RequestSpecHelper
     q.candidate_answers << API::IDVerification::Answer.new("None of the above")
     q.chosen_answer = "JANNE"
     questions << q
-    
+
     # question 2 with answers
     q = API::IDVerification::Question.new
     q.prompt = "TEST CHALLENGE - Which number goes with your address on CARVER BLVD?"
@@ -103,66 +109,66 @@ module RequestSpecHelper
     q.candidate_answers << API::IDVerification::Answer.new("None of the above")
     q.chosen_answer = "142"
     questions << q
-    
-    return questions    
+
+    return questions
   end
-  
+
 end
 
 module ResponseSpecHelper
-  
+
   def mock_error_response
     return File.read( File.dirname(__FILE__) + '/fixtures/error_response.xml' )
   end
-  
+
   def mock_unknown_response
     return File.read( File.dirname(__FILE__) + '/fixtures/unknown_response.xml' )
   end
-  
+
   def mock_no_match_response
     return File.read( File.dirname(__FILE__) + '/fixtures/no_match_response.xml' )
   end
-  
+
   def mock_match_found_response
     return File.read( File.dirname(__FILE__) + '/fixtures/match_found_response.xml' )
   end
-  
+
   def mock_match_found_single_address_response
     return File.read( File.dirname(__FILE__) + '/fixtures/match_found_single_address.xml' )
   end
-  
+
   def mock_match_found_ssn_does_not_match_response
     return File.read( File.dirname(__FILE__) + '/fixtures/match_found_ssn_does_not_match.xml' )
   end
-  
+
   def mock_match_found_ssn_invalid_response
     return File.read( File.dirname(__FILE__) + '/fixtures/match_found_ssn_invalid.xml' )
   end
-  
+
   def mock_match_found_ssn_issued_prior_to_dob_response
     return File.read( File.dirname(__FILE__) + '/fixtures/match_found_ssn_issued_prior_to_dob.xml' )
   end
-  
+
   def mock_match_found_ssn_unavailable_response
     return File.read( File.dirname(__FILE__) + '/fixtures/match_found_ssn_unavailable.xml' )
   end
-  
+
   def mock_match_found_subject_deceased_response
     return File.read( File.dirname(__FILE__) + '/fixtures/match_found_subject_deceased.xml' )
   end
-  
+
   def mock_match_found_thin_file_response
     return File.read( File.dirname(__FILE__) + '/fixtures/match_found_thin_file.xml' )
-  end  
-  
+  end
+
   def mock_questions_response
     return File.read( File.dirname(__FILE__) + '/fixtures/questions_response.xml' )
   end
-  
+
   def mock_verification_timeout_response
     return File.read( File.dirname(__FILE__) + '/fixtures/verification_timeout_response.xml' )
   end
-  
+
   def mock_verification_all_answers_correct_response
     return File.read( File.dirname(__FILE__) + '/fixtures/all_answers_correct_response.xml' )
   end
@@ -182,15 +188,15 @@ module ResponseSpecHelper
   def mock_challenge_questions_response
     return File.read( File.dirname(__FILE__) + '/fixtures/challenge_questions_response.xml' )
   end
-  
+
   def mock_challenge_verification_all_answers_correct_response
     return File.read( File.dirname(__FILE__) + '/fixtures/all_answers_correct_challenge_response.xml' )
   end
-  
+
   def mock_challenge_verification_1_answer_incorrect_response
     return File.read( File.dirname(__FILE__) + '/fixtures/one_answer_incorrect_challenge_response.xml' )
   end
-  
+
   def mock_challenge_verification_2_answers_incorrect_response
     return File.read( File.dirname(__FILE__) + '/fixtures/two_answers_incorrect_challenge_response.xml' )
   end
