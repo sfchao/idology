fe6f52d2a986c05e0540b7be19f0dcdff63778b0
Daniel Morrison <daniel@collectiveidea.com>
2009-12-04 12:48:57 +0000

    Removed old debugging helpers

--- lib/subject.rb
+++ lib/subject.rb
@@ -83,75 +83,7 @@ module IDology
       return false
     end
 
-
-    # for debugging
-    def set_match
-      # this is a test record that will be found
-      self.firstName = 'Spider'
-      self.lastName = 'Man'
-      self.address = '321 Orange Dr'
-      self.city = 'Miami'
-      self.state = 'FL'
-      self.zip = 33134
-      self.ssnLast4 = 1333
-      self.dobMonth = 1
-      self.dobYear = 1950
-
-      return "set to Spider Man"
-    end
-
-    def set_no_match
-      # this guy does not exist
-      self.firstName = 'DoesNot'
-      self.lastName = 'Exist'
-      self.address = '123 Main St'
-      self.city = 'Nowhere'
-      self.state = 'NY'
-      self.zip = 10001
-      self.ssnLast4 = 1234
-      self.dobMonth = 1
-      self.dobYear = 1965
-
-      return "set to DoesNot Exist"
-    end
-
-    def show_questions
-      # display the question.prompt and question.answer
-      if ! self.verification_questions.empty?
-        count = 0
-        self.verification_questions.each do |question|
-          puts count.to_s + " - " + question.prompt + ": \n"
-
-          question.answer.each do |answer|
-            puts "   - " + answer.text + "\n"
-          end
-
-          count += 1
-        end
-      end
-
-      return "\n"
-    end
-
-    def show_challenge_questions
-      # display the question.prompt and question.answer
-      if ! self.challenge_questions.empty?
-        count = 0
-        self.challenge_questions.each do |question|
-          puts count.to_s + " - " + question.prompt + ": \n"
-
-          question.answer.each do |answer|
-            puts "   - " + answer.text + "\n"
-          end
-
-          count += 1
-        end
-      end
-
-      return "\n"
-    end
-
-private
+  private
 
   def post(url, attributes)
     params = {:username => IDology[:username],
